1d136f9b5fa4c746dbd4d0c2cf6cb81e630d921f
nils-vanderwerf <n.vanderw.92@gmail.com>
2021-03-04 13:39:43 +1100

    Font Awesome fixed

---
+++ index.js
@@ -0,0 +1,12 @@
+function myFunc () {
+    const myVar = 42;
+
+    if (myVar > 10) {
+        console.log(myVar + ' is greater than 10');
+    }
+    return myVar * 2;
+}
+
+myVar;
+
+console.log(myVar);
